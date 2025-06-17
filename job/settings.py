928c2db1b897bcf3d9078b01ec157667a5a744de
MDJISHAN <103403364+MDJISHAN@users.noreply.github.com>
2025-06-17 18:43:57 +0530

    Update settings.py

--- job/settings.py
+++ job/settings.py
@@ -85,23 +85,14 @@ WSGI_APPLICATION = 'job.wsgi.application'
 
 
 #DATABASES = {
-  #  'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #   }
-#}
-# Database
-#https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'job_portal',
-        'USER': 'postgres',
-        'PASSWORD': 'Afreen@387',
-        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
-        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# Database
+#https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 #for debug toolbar
 INTERNAL_IPS = [
     # ...
